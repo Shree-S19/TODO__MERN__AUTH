e0a5debb56e161c1b7bc40526a2c8cbc4bedf89d
SHREE BALAJI <shreebalajisiva19@gmail.com>
2023-08-24 22:26:27 +0530

    Update Auth.jsx

--- Frontend/src/Pages/Auth.jsx
+++ Frontend/src/Pages/Auth.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Login from '../Components/auth/Login';
 import Register from '../Components/auth/Register';
 import Layout from '../Components/Layout';
-
+// need to update the Layout Components
 function Auth() {
   return (
     <Layout>
