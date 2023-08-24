0b971406b715592787fb1265b10f702760d8e46f
SHREE BALAJI <shreebalajisiva19@gmail.com>
2023-08-24 22:27:20 +0530

    Update Home.jsx

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const  Home = () =>{
   return (
     <div>
-      hiiii
+      Update Layout Component
     </div>
   )
 }
 
 export default Home
+// need to update the component of layouts
